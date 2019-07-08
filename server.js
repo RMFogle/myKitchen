19e22bc0686ed823d1ac0ddbac55f662907a8aab
obscura101 <vlad.neri@gmail.com>
2019-07-07 23:14:21 -0700

    fixed css and added login files

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 //set up handlebars
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 var isAuth 				 = require("./config/middleware/isAuthenticated");
