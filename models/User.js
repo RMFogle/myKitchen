da13c95a4ec6f7c174ad96251205fde4d85d7ff1
obscura101 <vlad.neri@gmail.com>
2019-07-09 13:46:44 -0700

    updated login

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ var bcrypt = require("bcrypt-nodejs");
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
-    email: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
