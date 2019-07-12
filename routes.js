c17df1ac5a53cadd3aad49b3162b932fa88b77bb
Drew26 <superdrewf26@hotmail.com>
2019-07-11 21:00:56 -0700

    delete html's

--- routes.js
+++ routes.js
@@ -5,17 +5,8 @@ module.exports = function(app){
     var kitchenA = require('./routes/kitchenA');
     var kitchenB = require('./routes/kitchenB');
     var users = require('./routes/users');
-<<<<<<< HEAD
-    //var kitchAdrygood = require('./routes/kitchAdrygood');
-    //var kitchAwalkFreeze = require('./routes/kitchAwalkFreeze');
-    //var kitchAwalkFridge = require('./routes/kitchAwalkFridge');
-    //var kitchBdrygood = require('./routes/kitchBdrygood');
-    //var kitchBwalkFreeze = require('./routes/kitchBwalkFreeze');
-    //var kitchBwalkFridge = require('./routes/kitchBwalkFridge');
-=======
     var kitchAdrygood = require('./routes/kitchAdrygood');
 
->>>>>>> be59720448e1d20e89abe29c4caffd1aeb8204a0
     
 
     app.use('/', application);
@@ -23,16 +14,7 @@ module.exports = function(app){
     app.use('/kitchenA', kitchenA);
     app.use('/kitchenB', kitchenB);
     app.use('/users', users);
-<<<<<<< HEAD
-    //app.use('/kitchAdrygood', kitchAdrygood);
-    //app.use('/kitchAwalkFreeze', kitchAwalkFreeze);
-    //app.use('/kitchAwalkFridge', kitchAwalkFridge);
-    //app.use('/kitchBdrygood', kitchBdrygood);
-    //app.use('/kitchBwalkFreeze', kitchBwalkFreeze);
-    //app.use('/kitchBwalkFridge', kitchBwalkFridge);
-=======
     app.use('/kitchAdrygood', kitchAdrygood);
->>>>>>> be59720448e1d20e89abe29c4caffd1aeb8204a0
     
 }
 
